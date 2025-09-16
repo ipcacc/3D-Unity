dd6700c0a2b29c5cbc099dfe81c4d1f0225f70ca
ipcacc <gims17315@gmail.com>
2025-09-16 16:02:29 +0900

    시네마신 카메라 기능 업데이트

--- Project/Assets/Script/PalyerMove.cs
+++ Project/Assets/Script/PalyerMove.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using Cinemachine;
+using Cinemachine.Utility;
 
 public class PalyerMove : MonoBehaviour
 {
@@ -11,6 +13,14 @@ public class PalyerMove : MonoBehaviour
 
     public float gravity = -9.81f;
 
+    public CinemachineVirtualCamera virtualCam;
+
+    public float rotationSpeed = 10f;
+
+    public CinemachineSwitcher CS;
+
+    private CinemachinePOV pov;
+
     private CharacterController controller;
 
     private Vector3 velocity;
@@ -19,17 +29,35 @@ public class PalyerMove : MonoBehaviour
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        pov = virtualCam.GetCinemachineComponent<CinemachinePOV>();
     }
 
     // Update is called once per frame
     void Update()
     {
         isGrounded = controller.isGrounded;
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = new Vector3(x, 0, z);
-        controller.Move(move * speed * Time.deltaTime);
+        Vector3 camForward = virtualCam.transform.forward;
+        camForward.y = 0;
+        camForward.Normalize();
+
+        Vector3 camRight = virtualCam.transform.right;
+        camRight.y = 0;
+        camRight.Normalize();
+
+        Vector3 move = (camForward * z + camRight * x).normalized;
+        if(!CS.usingFreeLook) 
+            controller.Move(move * speed * Time.deltaTime);
+
+        float cmaeraYaw = pov.m_HorizontalAxis.Value;
+        Quaternion targetRot = Quaternion.Euler(0f, cmaeraYaw, 0f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, rotationSpeed * Time.deltaTime);
 
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
@@ -37,5 +65,18 @@ public class PalyerMove : MonoBehaviour
         }
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = 10f;
+            virtualCam.m_Lens.FieldOfView = 80f;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = 5f;
+            virtualCam.m_Lens.FieldOfView = 60f;
+        }
+
+
     }
 }
